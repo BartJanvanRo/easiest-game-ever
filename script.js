a041f90f5083b4ecf447836afce14551c5f51f1a
BartJanvanRo <hjavanrossum@gmail.com>
2022-02-01 16:05:43 +0100

    1 feb 2

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-var counter=0;
-function jump(){
-    if(character.classList == "animate"){return}
+var counter = 0;
+
+const ArrayRound = ["a", "b", "c", "d"];
+
+
+function jump() {
+    if (character.classList == "animate") { return }
     character.classList.add("animate");
-    setTimeout(function(){
+    setTimeout(function () {
         character.classList.remove("animate");
-    },300);
+    }, 300);
 }
-var checkDead = setInterval(function() {
+var checkDead = setInterval(function () {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
         block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
+        alert("Game Over. score: " + Math.floor(counter / 100));
+        counter = 0;
         block.style.animation = "block 1s infinite linear";
-    }else{
+
+        block.innerHTML = ArrayRound[i];
+    } else {
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+        console.log(Math.floor(counter / 80))
+        let roundReturn = Math.floor(counter / 80)
+        block.innerHTML = ArrayRound[roundReturn];
+
+
+
+        /* block.innerHTML = roundReturn */
+
     }
 }, 10);
+
+
+
+
+//block
